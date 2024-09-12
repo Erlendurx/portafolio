8e0d61b17d9cc8d2ba5a6b31059f2bdffd2c5ea7
Erlendurx <erlanleon10@gmail.com>
2024-09-12 11:53:36 -0400

    Implementacion de textos animados de presentacion

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,13 +2,13 @@
 
 import React from 'react'
 import {motion} from 'framer-motion'
-import { slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/16/solid'
 
 const HeroContent = () => {
   return (
     <motion.div
-    initial="Hidden"
+    initial="hidden"
     animate="visible"
     className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
     >
@@ -18,8 +18,26 @@ const HeroContent = () => {
         className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'
         >
             <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-            <h1 className='Welcome-text text-[13px]'>Student Developer Portfolio</h1>
+            <h1 className='Welcome-text text-[13px]'>
+                Student Developer Portfolio
+            </h1>
         </motion.div>
+
+        <motion.div
+        variants={slideInFromLeft(0.5)}
+        className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+        >
+            Texto de prueba
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>antes de cerrar</span>
+            el proyecto
+        </motion.div>
+
+        <motion.p
+        variants={slideInFromLeft(0.8)}
+        className='text-lg text-gray-400 my-5 max-w-[600px]' 
+        >
+            Texto de prueba antes de cerrar el commit
+        </motion.p>
         </div>
     </motion.div>
   )
