f5f823c53faebb9625e6e866c1be92ee6a8be1bd
Erlendurx <erlanleon10@gmail.com>
2024-09-13 23:59:32 -0400

    Modificando el background de la pagina

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,11 +2,12 @@
 
 import React from 'react'
 import {motion} from 'framer-motion'
-import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/16/solid'
+import Image from 'next/image';
 
 const HeroContent = () => {
-  return (
+return (
     <motion.div
     initial="hidden"
     animate="visible"
@@ -15,7 +16,7 @@ const HeroContent = () => {
         <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
         <motion.div
         variants={slideInFromTop}
-        className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'
+        className='Welcome-box py-[8px] px-[8px] border border-[#7042f88b] opacity-[0.9]'
         >
             <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
             <h1 className='Welcome-text text-[13px]'>
@@ -25,11 +26,13 @@ const HeroContent = () => {
 
         <motion.div
         variants={slideInFromLeft(0.5)}
-        className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+        className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
         >
+            <span>
             Texto de prueba
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>antes de cerrar</span>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> antes de cerrar </span>
             el proyecto
+            </span>
         </motion.div>
 
         <motion.p
@@ -38,9 +41,28 @@ const HeroContent = () => {
         >
             Texto de prueba antes de cerrar el commit
         </motion.p>
+        <motion.a
+        variants={slideInFromLeft(1)}
+        className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w[200px]'
+        >
+            Texto de prueba
+        </motion.a>
         </div>
+
+        <motion.div
+        variants={slideInFromRight(0.8)}
+        className='w-full h-full flex justify-center items-center'
+        >
+
+            <Image
+                src="/mainIconsdark.svg"
+                alt="work icons"
+                height={650}
+                width={650}
+            />
+        </motion.div>
     </motion.div>
-  )
+)
 }
 
 export default HeroContent
