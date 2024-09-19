d9c19df9d66c6b4dc2b1dbdf28cc91b038fc8337
Erlendurx <erlanleon10@gmail.com>
2024-09-19 00:45:32 -0400

    Corrigiendo bugs el apartado de encriptacion

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 
 const Encryption = () => {
   return (
-    <div className='flex flex-row relative items-center justify-center w-full h-full'>
+    <div className='flex flex-row relative items-center justify-center min-h-screen w-full h-full'>
         <div className='absolute w-auto h-auto top-0 z-[5]'>
             <motion.div
             variants={slideInFromTop}
@@ -36,8 +36,16 @@ const Encryption = () => {
                     className='z-10'
                 />
             </div>
-        </div>
 
+            <div className='Welcome-box px-[15px] py-[4px] border my-[20px] border-[#7042f88b] opacity-[0.9]'>
+                <h1 className='Welcome-text text-[12px]'>Encryption</h1>
+            </div>
+        </div>
+        <div className='absolute z-[20] bottom-[10px] px-[5px]'>
+            <div className='cursive text-[20px] font-medium text-center text-gray-300'>
+                Asegurando tus datos de punta a punta
+            </div>
+        </div>
         <div className='w-full flex items-start justify-center absolute'>
             <video 
                 loop
